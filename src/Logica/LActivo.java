33067e6dcab6a33cdb0c585845a1fd3917141da2
Karinaporras <107158954+Karinaporras@users.noreply.github.com>
2022-07-10 16:28:03 -0600

    Bl empleados
    avance de empleados y auto

--- src/Logica/LActivo.java
+++ src/Logica/LActivo.java
@@ -1,22 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logica;
+
 import Clases.clsActivo;
 import Operaciones.OpActivos;
 import java.util.ArrayList;
+
 /**
  *
  * @author karii
  */
 public class LActivo {
-    //atributos
-  private int idActivo;
-  private clsActivo obActivo;
-  private OpActivos obOperaciones;
 
-  //get and set
+    private int idActivo;
+    private String codigo;
+    private clsActivo obActivo;
+    private OpActivos obOperaciones;
+
     public int getIdActivo() {
         return idActivo;
     }
@@ -24,6 +22,7 @@ public class LActivo {
     public void setIdActivo(int idActivo) {
         this.idActivo = idActivo;
     }
+
     public clsActivo getObActivo() {
         return obActivo;
     }
@@ -39,8 +38,8 @@ public class LActivo {
     public void setObOperaciones(OpActivos obOperaciones) {
         this.obOperaciones = obOperaciones;
     }
-//constructores
-    public LActivo (){
+
+    public LActivo() {
         this.obOperaciones = new OpActivos();
     }
 
@@ -48,31 +47,37 @@ public class LActivo {
         this.obActivo = obActivo;
         this.obOperaciones = new OpActivos();
     }
-    public LActivo(int idActivo){
+
+    public LActivo(int idActivo) {
         this.idActivo = idActivo;
         this.obOperaciones = new OpActivos();
-        
+
     }
-    
-  public boolean Guardar() {
-      return this.obOperaciones.Guardar(this.obActivo);
-      
-  }
- 
-  public boolean Actualizar(){
-      return this.obOperaciones.Actualizar(this.obActivo);
-      
-  }
-  public clsActivo Consultar(){
-      return this.obOperaciones.Consultar(this.idActivo);
-      
-  }
-  public boolean Eliminar(){
-      return this.obOperaciones.Eliminar(this.idActivo);
-      
-  }
-  public ArrayList<clsActivo> Listar(){
-      return this.obOperaciones.Listar();
-      
-  }
+
+    public boolean Guardar() {
+        return this.obOperaciones.Guardar(this.obActivo);
+
+    }
+
+    public boolean Actualizar() {
+        return this.obOperaciones.Actualizar(this.obActivo);
+
+    }
+
+    public clsActivo Consultar() {
+        if (this.idActivo > 0) {
+            return this.obOperaciones.Consultar(this.idActivo);
+        } else {
+            return this.obOperaciones.Consultar(this.codigo);
+        }
+    }
+
+    public boolean Eliminar() {
+        return this.obOperaciones.Eliminar(this.idActivo);
+    }
+
+    public ArrayList<clsActivo> Listar() {
+        return this.obOperaciones.Listar();
+    }
+
 }
---
+++ src/Logica/LEmpleados.java
@@ -0,0 +1,70 @@
+package Logica;
+
+import Clases.clsEmpleado;
+import Operaciones.OpEmpleados;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author karii
+ */
+public class LEmpleados {
+
+    private int idEmpleado;
+    private String codigo;
+    private clsEmpleado obEmpleado;
+    private OpEmpleados obOperaciones;
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public LEmpleados() {
+        this.obOperaciones = new OpEmpleados();
+    }
+
+    public LEmpleados(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+        this.obOperaciones = new OpEmpleados();
+
+    }
+
+    public LEmpleados(String codigo) {
+        this.codigo = codigo;
+        this.obOperaciones = new OpEmpleados();
+    }
+
+    public LEmpleados(clsEmpleado obEmpleado) {
+        this.obEmpleado = obEmpleado;
+        this.obOperaciones = new OpEmpleados();
+    }
+
+    public boolean Guardar() {
+        return this.obOperaciones.Guarda(this.obEmpleado);
+    }
+
+    public boolean Actualizar() {
+        return this.obOperaciones.Actualizar(this.obEmpleado);
+    }
+
+    public boolean Eliminar() {
+        return this.obOperaciones.Eliminar(this.idEmpleado);
+    }
+
+    public clsEmpleado Consultar() {
+        if (this.idEmpleado > 0) {
+            return this.obOperaciones.Consultar(this.idEmpleado);
+        } else {
+            return this.obOperaciones.Consultar(this.codigo);
+
+        }
+    }
+
+    public ArrayList<clsEmpleado> Listar() {
+        return this.obOperaciones.Listar();
+    }
+}
