469e005058bd776c7f0d5e4208c216a85bfce303
Karinaporras <107158954+Karinaporras@users.noreply.github.com>
2022-07-12 21:16:06 -0600

    terminado
    Se terminó totalmente BL

--- src/Interfaces/intzGestor.java
+++ src/Interfaces/intzGestor.java
@@ -2,6 +2,8 @@
 package Interfaces;
 
 import Clases.clsActivo;
+import Clases.clsAutos;
+import Clases.clsEmpleado;
 import java.util.ArrayList;
 
 
@@ -12,10 +14,25 @@ import java.util.ArrayList;
  */
 public interface intzGestor {
 
-    
+    //Activos
     public boolean GuardarActivo(clsActivo obActivo);
     public boolean ActualizarActivo (clsActivo obActivo);
     public boolean EliminarActivo(int idActivo);
     public clsActivo ConsultarActivo(int idActivo);
     public ArrayList<clsActivo> ListarActivos();
+    
+    // Autos 
+     public boolean GuardarAutos(clsAutos obAuto);
+     public boolean ActualizarAutos(clsAutos obAutos);
+     public boolean EliminarAutos(int idAuto);
+     public clsAutos ConsultarAutos(int idAutos);
+     public ArrayList<clsAutos> ListarAutos();
+     
+     // Empleados
+     public boolean GuardarEmpleado(clsEmpleado obEmpleado);
+     public boolean ActualizarEmpleado(clsEmpleado obEmpleado);
+     public boolean EliminarEmpleado(int idEmpleado);
+     public clsEmpleado ConsultarEmpleado(int idEmpleado);
+     public ArrayList<clsEmpleado> ListarEmpleado();
+     
 }
--- src/Seguridad/clsGestor.java
+++ src/Seguridad/clsGestor.java
@@ -1,9 +1,10 @@
-
 package Seguridad;
 
 import Clases.clsActivo;
 import Interfaces.intzGestor;
 import Logica.*;
+import Clases.clsAutos;
+import Clases.clsEmpleado;
 import java.util.ArrayList;
 
 /**
@@ -11,8 +12,10 @@ import java.util.ArrayList;
  * @author karii
  */
 public class clsGestor implements intzGestor {
+
     private LActivo myActivo;
-    
+    private LAuto myAuto;
+    private LEmpleados myEmpleados;
 
     @Override
     public boolean GuardarActivo(clsActivo obActivo) {
@@ -28,20 +31,80 @@ public class clsGestor implements intzGestor {
 
     @Override
     public boolean EliminarActivo(int idActivo) {
-         this.myActivo = new LActivo(idActivo);
+        this.myActivo = new LActivo(idActivo);
         return this.myActivo.Eliminar();
     }
 
     @Override
     public clsActivo ConsultarActivo(int idActivo) {
-       this.myActivo = new LActivo(idActivo);
+        this.myActivo = new LActivo(idActivo);
         return this.myActivo.Consultar();
     }
 
     @Override
     public ArrayList<clsActivo> ListarActivos() {
-      this.myActivo = new LActivo();
-      return this.myActivo.Listar();
+        this.myActivo = new LActivo();
+        return this.myActivo.Listar();
     }
-    
+
+    @Override
+    public boolean GuardarAutos(clsAutos obAuto) {
+        this.myAuto = new LAuto(obAuto);
+        return this.myAuto.Guardar();
+    }
+
+    @Override
+    public boolean ActualizarAutos(clsAutos obAutos) {
+        this.myAuto = new LAuto(obAutos);
+        return this.myAuto.Actualizar();
+    }
+
+    @Override
+    public boolean EliminarAutos(int idAuto) {
+        this.myAuto = new LAuto(idAuto);
+        return this.myAuto.Eliminar();
+    }
+
+    @Override
+    public clsAutos ConsultarAutos(int idAutos) {
+        this.myAuto = new LAuto(idAutos);
+        return this.myAuto.Consultar();
+    }
+
+    @Override
+    public ArrayList<clsAutos> ListarAutos() {
+        this.myAuto = new LAuto();
+        return this.myAuto.Listar();
+    }
+
+    @Override
+    public boolean GuardarEmpleado(clsEmpleado obEmpleado) {
+        this.myEmpleados = new LEmpleados(obEmpleado);
+        return this.myEmpleados.Guardar();
+    }
+
+    @Override
+    public boolean ActualizarEmpleado(clsEmpleado obEmpleado) {
+        this.myEmpleados = new LEmpleados(obEmpleado);
+        return this.myEmpleados.Actualizar();
+    }
+
+    @Override
+    public boolean EliminarEmpleado(int idEmpleado) {
+        this.myEmpleados = new LEmpleados(idEmpleado);
+        return this.myEmpleados.Eliminar();
+    }
+
+    @Override
+    public clsEmpleado ConsultarEmpleado(int idEmpleado) {
+        this.myEmpleados = new LEmpleados(idEmpleado);
+        return this.myEmpleados.Consultar();
+    }
+
+    @Override
+    public ArrayList<clsEmpleado> ListarEmpleado() {
+        this.myEmpleados = new LEmpleados();
+        return this.myEmpleados.Listar();
+    }
+
 }
